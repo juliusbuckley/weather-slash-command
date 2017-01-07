c43832896538f1488bd348cbec0ddacdca577089
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 17:03:21 -0500

    feat/ added pem to manage cert

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import http from 'http';
 import https from 'https';
+import pem from 'pem';
 import fs from 'fs';
 import cors from 'cors';
 import typeahead from './api/typeahead';
@@ -20,11 +21,17 @@ const corsOptions = {
   credentials: true
 };
 
-// API ROUTES
-app.get('/typeahead', cors(corsOptions), typeahead);
-app.get('/resolver', cors(corsOptions), resolver);
-
 // create http service
-http.createServer(app).listen(port, () => console.log(`app.js has been served on port: ${port}`));
-// create https service
-https.createServer((options, app)).listen(443, () => console.log(`app.js has been served on port: 443`));
+// http.createServer(app).listen(port, () => console.log(`app.js has been served on port: ${port}`));
+// https.createServer((options, app)).listen(443, () => console.log(`app.js has been served on port: 443`));
+
+pem.createCertificate({days: 1, selfSigned: true}, (err, keys) => {
+  var app = express();
+
+  // API ROUTES
+  app.get('/typeahead', cors(corsOptions), typeahead);
+  app.get('/resolver', cors(corsOptions), resolver);
+
+  // create https service
+  https.createServer({key: keys.serviceKey, cert: keys.certificate}, app).listen(443);
+});
