95b1ea234b37f1bcb913a7bd6af6092f57a40539
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 15:01:41 -0500

    feat/ set up routes and added cors options

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import apiRoutes from './routes/apiRoutes';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import typeahead from './api/typeahead';
+import resolver from './api/resolver';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-apiRoutes(app);
+const corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
+
+// API ROUTES
+app.get('/typeahead', cors(corsOptions), typeahead);
+app.get('/resolver', cors(corsOptions), resolver);
 
 app.listen(port, () => { console.log(`app.js has been served on port: ${port}`); });
