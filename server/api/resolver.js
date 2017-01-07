56bc009bbccec282e4167f413370c29861255a14
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 18:51:51 -0500

    chores/ updated url

--- server/api/resolver.js
+++ server/api/resolver.js
@@ -19,16 +19,17 @@ export default function(req, res) {
       const icon = response.data.weather[0].icon + 'png';
       const imageUrl = 'https://192.241.218.147/images/01d.png';
       const width = 600;
-      const test = `<img style="max-width:100%;" src="${imageUrl}" width="${width}"/>`;
-      const html = [
+      const test1 = `<img style="max-width:100%;" src="${imageUrl}" width="${width}"/>`;
+      const html1 = [
         `<img src=/images/${icon}>
         <span>Temp: ${data.main.temp}</span>
         <span>High: ${data.main.temp_max}</span>
         <span>Low: ${data.main.temp_min}</span>
         <span>Description: ${data.weather[0].description}</span>`
       ].join('');
+      const html = '<img style="max-width:100%;" src="https://www.rover.com/blog/wp-content/uploads/2015/05/dog-candy-junk-food-599x340.jpg" width="' + width + '"/>';
       res.json({
-        body: test
+        body: html
       });
     })
     .catch((err) => { console.log(`Error: ${err}`); });
