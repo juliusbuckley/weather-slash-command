44e509a6e1cfaa8e90e5f40a5b8b8fdc3c1c25af
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 15:02:29 -0500

    chores/ deleted modular path

--- server/routes/apiRoutes.js
+++
@@ -1,5 +0,0 @@
-import searchController from '../controllers/searchController';
-// API ROUTES
-const apiRoutes = (app) => { app.get('/typeahead', searchController); };
-const apiRoutes = (app) => { app.get('/resolver', searchController); };
-export default apiRoutes;
