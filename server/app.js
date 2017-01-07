7ac7c83a23d8dc77aa24b9d883682ae2cc15f8c0
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 16:34:50 -0500

    chores/ added http and https server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import http from 'http';
 import https from 'https';
 import fs from 'fs';
 import cors from 'cors';
@@ -9,20 +10,21 @@ import resolver from './api/resolver';
 const app = express();
 const port = process.env.PORT || 8080;
 
-const corsOptions = {
-  origin: /^[^.\s]+\.mixmax\.com$/,
-  credentials: true
-};
-
 const options = {
   key: fs.readFileSync('../keys/key.pem'),
   cert: fs.readFileSync('../keys/cert.pem')
 };
 
-https.createServer((options, app)).listen(8080);
+const corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
 
 // API ROUTES
 app.get('/typeahead', cors(corsOptions), typeahead);
 app.get('/resolver', cors(corsOptions), resolver);
 
-// app.listen(port, () => { console.log(`app.js has been served on port: ${port}`); });
+// create http service
+http.createServer(app).listen(port, () => console.log(`app.js has been served on port: ${port}`));
+// create https service
+https.createServer((options, app)).listen(443, () => console.log(`app.js has been served on port: 443`));
