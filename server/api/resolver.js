72c183666b6d252296fb9e3776ed4f459488dc5b
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 20:04:49 -0500

    chores/ changed image alignment

--- server/api/resolver.js
+++ server/api/resolver.js
@@ -17,9 +17,9 @@ export default function(req, res) {
     .then((response) => {
       const data = response.data;
       const icon = data.weather[0].icon;
-      const width = 300;
+      const width = 100;
       res.json({
-        body: '<img style="max-width:100% vertical-align:middle" src="https://192.241.218.147/images/' + icon + '.png" height="300" width="' + width + '"/> \n' + '<span>Temp: ' + Math.floor(data.main.temp) + '</span>\n<span>Description: ' + data.weather[0].description + '</span>'
+        body: '<img style="max-width:100% vertical-align:text-bottom" src="https://192.241.218.147/images/' + icon + '.png" height="100" width="' + width + '"/> \n' + '<span>Temp: ' + Math.floor(data.main.temp) + '</span>\n<span>Description: ' + data.weather[0].description + '</span>'
       });
     })
     .catch((err) => { console.log(`Error: ${err}`); });
