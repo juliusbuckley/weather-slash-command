5ccfd4ebc772eea56d85f69ea6f199b00dd10a44
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 19:20:03 -0500

    chores/ template

--- server/api/resolver.js
+++ server/api/resolver.js
@@ -18,9 +18,7 @@ export default function(req, res) {
       const data = response.data;
       const icon = data.weather[0].icon + 'png';
       const width = 600;
-      const html = [
-        `<img style="max-width:100%;" src="https://192.241.218.147/images/${icon}" width="${width}"/><span>Temp: ${data.main.temp}</span><span>High: ${data.main.temp_max}</span><span>Low: ${data.main.temp_min}</span><span>Description: ${data.weather[0].description}</span>`
-      ].join('');
+      const html = `<img style=max-width:100%; src=https://192.241.218.147/images/${icon} width="${width}"/> <span>Temp: ${data.main.temp}</span><span> High: ${data.main.temp_max}</span><span> Low: ${data.main.temp_min}</span><span>Description: ${data.weather[0].description}</span>`;
       res.json({
         body: html
       });
