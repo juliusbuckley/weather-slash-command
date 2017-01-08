e737994f3e56294d42f7e0dc16d3dfe19f4dbf68
juliusbuckley <juliusbuckley@gmail.com>
2017-01-07 19:37:44 -0500

    chores/ template

--- server/api/resolver.js
+++ server/api/resolver.js
@@ -21,7 +21,7 @@ export default function(req, res) {
       const imageUrl = '<img style="max-width:100%;" src="https://192.241.218.147/images/' + icon + '" width="' + width + '"/>';
       const html = `<img style=max-width:100%; src=https://192.241.218.147/images/${icon} width="${width}"/> <span>Temp: ${data.main.temp}</span><span> High: ${data.main.temp_max}</span><span> Low: ${data.main.temp_min}</span><span> Description: ${data.weather[0].description}</span>`;
       res.json({
-        body: imageUrl
+        body: '<img style="max-width:100%;" src="' + 'https://192.241.218.147/images/' + icon + ' + " width="' + width + '"/> + temp + description'
       });
     })
     .catch((err) => { console.log(`Error: ${err}`); });
