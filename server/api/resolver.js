b25b93a770236929157f591cc7aee338a0cb1f75
juliusbuckley <juliusbuckley@gmail.com>
2017-01-12 12:18:04 -0800

    Add degree symbol

--- server/api/resolver.js
+++ server/api/resolver.js
@@ -17,7 +17,7 @@ export default function(req, res) {
       const data = response.data;
       const icon = data.weather[0].icon;
       res.json({
-        body: '<img style="max-width:100% vertical-align:text-bottom" src="https://192.241.218.147/images/' + icon + '.png" height="100" width="100"/> ' + '<span font-size : x-large> <br>Location: ' + data.name + ', ' + data.sys.country + '</span> <span font-size : x-large> <br>Temp: ' + Math.floor(data.main.temp) + '</span> <span font-size : x-large> <br>Description: ' + data.weather[0].description + '</span>'
+        body: '<img style="max-width:100% vertical-align:text-bottom" src="https://192.241.218.147/images/' + icon + '.png" height="100" width="100"/> ' + '<span font-size : x-large> <br>Location: ' + data.name + ', ' + data.sys.country + '</span> <span font-size : x-large> <br>Temp: ' + Math.floor(data.main.temp) + '&deg f' + '</span> <span font-size : x-large> <br>Description: ' + data.weather[0].description + '</span>'
       });
     })
     .catch((err) => { console.log(`Error: ${err}`); });
